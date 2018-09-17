d3d8cbe60fcdea87d44a7eb5afada8fb79f720ab
pimprenelle <nico2.vincent@gmail.com>
2018-09-17 17:23:03 +0200

    Update in order to add psy mode.

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Il contient le code source du site suivant : [https://pimprenelie.github.io](htt
 
 Au 13 septembre 2018, le code source est une copie du site "bettermotherfuckingwebsite" recréé comme projet du cours de [The Hacking Project](https://www.thehackingproject.org/) pour découvrir le HTML5 et le CSS3.
 
-Au 17 septembre 2018, le site a été complété par l'ajout de Javascript. Plus précisément une pop-up active après 10sec sur le site.
+Au 17 septembre 2018, le site a été complété par l'ajout de Javascript. Plus précisément une pop-up active après 10sec sur le site. Un bouton a aussi été ajouté permettant de passer le site dans un mode psychadélique.
--- scripts/main.js
+++ scripts/main.js
@@ -12,16 +12,72 @@ window.onload = function() {
 }*/
 
 
+
 //*-*-*-*-*-*-*-*SECOND POP-UP*-*-*-*-*-*-*-*
+
+/*DECLARATION*/
+
 // Get the modal
 var modal = document.getElementById('myModal');
 
 //Get the overlay
 var overlay = document.getElementById('modalOverlay');
 
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
+// Get the button that opens the modal
+var btn = document.getElementById("popupButton");
+
+// Get the mode of the window
+var mode = localStorage.getItem('mode')
+
+// Get the button that make psychadelic mode
+var btnPsy = document.getElementById("psyMode");
+
+// Get all paragraph
+var paragraph = document.getElementsByTagName('p');
+
+
+
+var headings = document.querySelectorAll('h1,h2,h3');
+
+/*FUNCTION*/
+
+//Changer le style pour le mettre en mode psychadélique
+function psyMode(){
+    mode='psy';
+    localStorage.setItem('mode', mode);
+    //Ici, mettre style à jour
+    document.body.style.backgroundColor='#f0f';
+    for(let i=0;i<paragraph.length;i++){
+        paragraph[i].style.color='#00f';
+        paragraph[i].style.fontFamily='Papyrus';
+    }
+    for(let i=0;i<headings.length;i++){
+        headings[i].style.color='#0f0';
+        headings[i].style.fontFamily='Comic Sans';
+    }
+}
+
+//Changer le style pour retourner au mode standard (noir sur blanc)
+function standardMode(){
+    mode='standard';
+    localStorage.setItem('mode',mode);
+    //Ici remettre style standard
+    document.body.style.backgroundColor='#fff';
+
+    for(let i=0;i<paragraph.length;i++){
+        paragraph[i].style.color='#444';
+        paragraph[i].style.fontFamily='serif';
+    }
+
+    for(let i=0;i<headings.length;i++){
+        headings[i].style.color='#444';
+        headings[i].style.fontFamily='serif';
+    }
+
+}
 
 // Function which loads the pop-up
 var myFunc = function() {
@@ -29,17 +85,22 @@ var myFunc = function() {
     modal.style.display = "block";
 }
 
-// Get the button that opens the modal
-var btn = document.getElementById("popupButton");
-
-//Execute the funciton "myFunc" after 10s the page is load.
-window.onload = function() {
-  setTimeout(myFunc, 10000);
+//Function which open a link
+function openLink(url){
+    document.location.href = url;
+    return true;
 }
 
-function openLink(url){
-document.location.href = url;
-return true;
+
+/*EXECUTION*/
+
+//Execute the function "myFunc" after 10s the page is load.
+//Execute the function psyMode if the user set in psy mode.
+window.onload = function() {
+    setTimeout(myFunc, 10000);
+    if(mode=='psy'){
+        psyMode();
+    }
 }
 
 btn.onclick = function() {
@@ -60,4 +121,11 @@ window.onclick = function(event) {
     }
 }
 
-
+btnPsy.onclick=function(){
+    if(mode=='psy'){
+        standardMode();
+    }
+    else{
+        psyMode();
+    }
+}
